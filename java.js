cac25f03d58cbe75b94969264ee152c51bbacd3c
Dominik Kloc <dominic2156@hotmail.com>
2022-09-21 09:15:21 +0100

    create operator functions

--- java.js
+++ java.js
@@ -1,14 +1,62 @@
-function add(a, b) {
-    return a + b;
-};
-function substract(a, b) {
-    return a - b;
+let currentNum = "";
+let operator = "";
+let previousNum = "";
+
+const operatorBtn = document.querySelectorAll('.operator');
+const equalBtn = document.querySelector('.equal');
+const numberBtn = document.querySelectorAll('.number');
+const clearBtn = document.querySelector('.clear');
+const currentDisplay = document.querySelector('.currentDisplay');
+const previousDisplay = document.querySelector('.previousNum');
+
+numberBtn.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+        handleNumber(e.target.textContent);
+    });
+});
+
+function handleNumber(number) {
+    if (currentNum.length <= 4) {
+        currentNum += number;
+        currentDisplay.textContent = currentNum;
+    }
 };
 
-function multiply (a, b) {
-    return a * b;
+
+operatorBtn.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+        handleOperator(e.target.textContent);
+    });
+});
+
+function handleOperator(op) {
+    operator = op;
+    previousNum = currentNum;
+    previousDisplay.textContent = previousNum + operator;
+    currentNum = "";
+    currentDisplay.textContent = "";
 };
 
-function divide(a, b) {
-    return (a / b);
+function add(previousNum, currentNum) {
+    return previousNum + currentNum;
 };
+function substract(previousNum, currentNum) {
+    return previousNum - currentNum;
+};
+
+function multiply (previousNum, currentNum) {
+    return previousNum * currentNum;
+};
+
+function divide(previousNum, currentNum) {
+    return (previousNum / currentNum);
+};
+
+equalBtn.addEventListener('click', (e) => {
+    calculate(e.target.textContent);
+});
+
+//calculate() {
+  //  previousNum = Number(previousNum);
+  //  currentNum = Number(currentNum);
+  
